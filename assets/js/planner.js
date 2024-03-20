1d3f826fca3e69c241b48503bf0355af50cd5424
rthomas86 <rebkthomas72@gmail.com>
2024-03-19 20:39:07 -0700

    added local storage to save users

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,11 +1,20 @@
 function userInfo() {
+  var login = [];
   var user = $(".user").val();
   var password = $(".password").val();
   $(".user").val("");
   $(".password").val("");
+  newUser = [{ user: user, password: password }];
+  console.log(newUser);
+  login.push(newUser);
 
-  alert(user + " " + password);
+  $(this).modal("hide");
+  console.log(login);
+  localStorage.setItem("signup", JSON.stringify(login));
 }
 
-login = [];
 $(".new_user").on("click", userInfo);
+
+login = JSON.parse(localStorage.getItem("signup"));
+console.log(login);
+console.log(localStorage.length);
